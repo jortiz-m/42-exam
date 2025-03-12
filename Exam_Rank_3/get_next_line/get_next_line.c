709a7379b84e3678724b27571b028087904d32a3
Julia Ortiz <166491860+jortiz-m@users.noreply.github.com>
2025-03-12 10:58:31 +0000

    v7.1

--- Exam_Rank_3/get_next_line/get_next_line.c
+++ Exam_Rank_3/get_next_line/get_next_line.c
@@ -1,60 +1,58 @@
 #include <unistd.h> //read
 #include <stdio.h> //printf
 #include <stdlib.h> //malloc
-#include <fcntl.h> //fd
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
 #endif
 
-
-char *ft_strdup(char *line)
+char    *ft_strdup(char *str)
 {
-	char *new;
-	int i = 0;
-	while(line[i])
-		i++;
-	new = malloc((i + 1)* sizeof(char *));
-	if(!new)
-		return(NULL);
-	i = 0;
-	while(line[i])
-	{
-		new[i] = line[i];
-		i++;
-	}
-	new[i] = '\0';
-	return(new);
+        char    *new;
+        int     i;
+
+        i = 0;
+        while(str[i])
+                i++;
+        new = malloc((i + 1) * sizeof(char*));
+        i = 0;
+        while(str[i])
+        {
+                new[i] = str[i];
+                i++;
+        }
+        new[i] = '\0';
+        return(new);
 }
 
-char *get_next_line(int fd)
+char    *get_next_line(int fd)
 {
-	char			line[70000]; //el que devuelvo
-	static char		buffer[BUFFER_SIZE]; //donde hago la lectura
-	static int 		buffer_read; //lo que leemos
-	static int		buffer_pos; //lo que hemos copiado
-	int i;
+        char            line[70000];
+        static char     buffer[BUFFER_SIZE];
+        static int      buffer_copy;
+        static int      buffer_read;
+        int             i;
 
-	i = 0;
-	if(fd < 0 || BUFFER_SIZE <= 0)
-		return(NULL);
-	while(1)
-	{
-		if(buffer_pos >= buffer_read)
-		{
-			buffer_read = read(fd, buffer, BUFFER_SIZE);
-			buffer_pos = 0;
-			if(buffer_read <= 0)
-				break;
-		}
-		if(line[i - 1] == '\n')
-			break;
-		line[i] = buffer[buffer_pos];
-		i++;
-		buffer_pos++;
-	}
-	line[i] = '\0';
-	if(i == 0)
-		return(NULL);
-	return(ft_strdup(line));
+        i = 0;
+        if(fd < 0 || BUFFER_SIZE <= 0)
+                return(NULL);
+        while(1)
+        {
+                if(buffer_copy >= buffer_read)
+                {
+                        buffer_read = read(fd, buffer, BUFFER_SIZE);
+                        buffer_copy = 0;
+                        if(buffer_read <= 0)
+                                break ;
+                }
+                if(line[i - 1] == '\n')
+                        break ;
+                line[i] = buffer[buffer_copy];
+                i++;
+                buffer_copy++;
+        }
+        line[i] = '\0';
+        if(i == 0)
+                return (NULL);
+        return(ft_strdup(line));
 }
